9baf6610f91a6436429560238ac98c3dd3e343de
KINGALVI <alvialmahamud@gmail.com>
2023-04-13 13:52:51 +0600

    edit some code

--- alvi.js
+++ alvi.js
@@ -1,4 +1,4 @@
-//largest number
+//largest Number Math
 
 const lagest = [673, 3747, 574, 2748, 375, 3893, 373, 5873, 500, 8558];
 
@@ -25,11 +25,12 @@ const lagestResult = lagestSum(lagest);
 
 
 
-//fibo math
+//fibo Math
+
 const fibo = [0, 1];
 
 for (let i = 2; i <= 12; i++) {
-    fibo[i] = fibo[i - 1] + fibo[i - 2];
+  fibo[i] = fibo[i - 1] + fibo[i - 2];
 }
 
 // console.log(fibo);
@@ -38,21 +39,21 @@ for (let i = 2; i <= 12; i++) {
 
 
 
-// includs example ;
+// Includs example ;
 
 const names = ['abul', 'babul', 'cabul', 'dabul', 'ebul', 'babul', 'kabul', 'gabul', 'cabul', 'babul', 'abul', 'abul'];
 
 function removeDuplicate(names) {
-    const unique = [];
+  const unique = [];
 
-    for (let i = 0; i < names.length; i++) {
-        const element = names[i];
-        if (unique.includes(element) === false) {
-            unique.push(element);
-        }
+  for (let i = 0; i < names.length; i++) {
+    const element = names[i];
+    if (unique.includes(element) === false) {
+      unique.push(element);
     }
+  }
 
-    return unique;
+  return unique;
 }
 
 const newNameList = removeDuplicate(names);
@@ -64,17 +65,42 @@ const newNameList = removeDuplicate(names);
 
 //foo + bar = foobar 
 
-for(let i = 0; i <= 50; i++){
-  if (i % 3 === 0 && i % 5 === 0 ){
-    console.log('foobar');
-   }
- else if(i % 3=== 0){
-  console.log('bar');
- }
- else if (i % 5 === 0 ){
-  console.log('foobar');
- }
-  else{
-    console.log(i);
+for (let i = 0; i <= 50; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    // console.log('foobar');
+  }
+  else if (i % 3 === 0) {
+    // console.log('bar');
+  }
+  else if (i % 5 === 0) {
+    // console.log('foobar');
+  }
+  else {
+    // console.log(i);
   }
 }
+
+
+
+
+// Wood Calculator Math
+
+const totalChair = 5;
+const totalTable = 10;
+const totalBed = 15;
+
+function woodCalculator(chairQuantity, tableQuantity, bedQantity) {
+
+  const perChairQuantity = chairQuantity * 3;
+  // console.log(`total Chair woood requerment is ${perChairQuantity}cft`);
+
+  const perTableQuantity = tableQuantity * 10;
+  // console.log(`total Tabel woood requerment is ${perTableQuantity}cft`);
+
+  const perBedQuantity = bedQantity * 50;
+  // console.log(`total Tabel woood requerment is ${perBedQuantity}cft`);
+
+}
+
+
+woodCalculator(totalChair, totalTable, totalBed);
